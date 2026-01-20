0abbf42aa5e0fd97fb41f7ecee1ebe27a8103ccf
Ravi Yadav <88570593+RaviYadav369@users.noreply.github.com>
2026-01-20 12:10:46 +0000

    update code

--- gridlight/src/App.js
+++ gridlight/src/App.js
@@ -1,24 +1,13 @@
-import { useState } from 'react';
 import './App.css';
 import GridLights from './components/grid';
 
 function App() {
-  const [gridLen, setgridLen] = useState()
-  const [num, setN] = useState()
-
-  const handleIn = (e) => setN(e.target.value)
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(num)
-    setgridLen(num)
-  }
 
   return (
     <div className="App">
       <h2>Grid Lights</h2>
     
-      <GridLights n={gridLen} />
+      <GridLights />
     </div>
   );
 }
